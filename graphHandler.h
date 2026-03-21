16c297b5f9c31b2f53676000c95991a2944569a4
Antoine Richard <antoine.richard972@gmail.com>
2026-03-21 17:32:39 +0100

    Modified functions
    Problem on adjacency matrix:
    when value = 0 for no link, if weight = 0 then the link is not considered
    
    solution: put INF instead of 0
    
    Had to modify the floydWarshall.c to suit this new condition

--- graphHandler.h
+++ graphHandler.h
@@ -15,7 +15,8 @@ typedef struct graph GRAPH;
 GRAPH* createGraph(int nbVertices, int nbArcs);
 
 /**
- * Create a graph from a file
+ * Create a graph from a file (creation of adjacency matrix of a directed weighted graph)
+ * if edge doesnt exist, we set to INF the value to consider weight = 0
  * @return A n by n matrix (n the number of vertices)
  */
 GRAPH* createGraphFromFile(char* fileName);
@@ -25,5 +26,8 @@ GRAPH* createGraphFromFile(char* fileName);
  */
 void displayMatrix(int** matrix, int nbVertices);
 
+void freeGraph(GRAPH* graph);
+
+void freeMatrix(int** matrix, int n);
 
 #endif //SHORTESTPATH_GRAPHHANDLER_H
